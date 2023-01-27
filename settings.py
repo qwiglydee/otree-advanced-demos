eb439e88a70362e3af5796c49ae39a4430f935be
QwiglyDee <qwiglydee@gmail.com>
2023-01-27 23:33:06 +0700

    cleanup

--- settings.py
+++ settings.py
@@ -1,28 +1,11 @@
 from os import environ
 
 SESSION_CONFIGS = [
-   dict(
-       name="demo_chat",
-       display_name="multi-player chat",
-       num_demo_participants=3,
-       app_sequence=['chat']
-   ),
-#    dict(
-#        name="sliders",
-#        display_name="classic sliders task",
-#        num_demo_participants=1,
-#        app_sequence=['sliders']
-#    ),
-#    dict(
-#        name="demo_task",
-#        display_name="demo task app: solving math",
-#        num_demo_participants=1,
-#        app_sequence=['demo_task']
-#    ),
 ]
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+    real_world_currency_per_point=1.00,
+    participation_fee=0.00, doc=""
 )
 
 LANGUAGE_CODE = "en"
