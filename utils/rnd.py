c9a852aa5630aa94cf97832600217d498a3212d6
QwiglyDee <qwiglydee@gmail.com>
2024-08-25 09:57:53 +0700

    cleanup

--- utils/rnd.py
+++ utils/rnd.py
@@ -5,12 +5,21 @@ def bernoulli(p):
     return random.random() < p
 
 
+def coin():
+    return bernoulli(0.5)
+
+
 def binomial(p, n):
     return (bernoulli(p) for _ in range(n))
 
 
 def bernulchoice(p, a, b):
     return a if bernoulli(p) else b
+
+
+def multinulchoice(probs, vals):
+    [res] = random.choices(vals, probs, k=1)
+    return res
 
 
 def shuffled(lst: list):
